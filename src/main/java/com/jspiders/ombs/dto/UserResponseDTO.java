48c51f6f660df7d908721677c16221ec40040a1d
Deepika0142 <deepikagowdak2410@gmail.com>
2023-09-12 18:01:26 +0530

    webservice to register

---
+++ src/main/java/com/jspiders/ombs/dto/UserResponseDTO.java
@@ -0,0 +1,70 @@
+package com.jspiders.ombs.dto;
+
+import java.time.LocalDateTime;
+
+public class UserResponseDTO {
+	private int userId;
+	private String userFirstName;
+	private String userLastName;
+	private String userRole;
+	private String userEmail;
+	private LocalDateTime createdDate;
+	private String createdBy;
+	private LocalDateTime updatedDate;
+	private String updatedBy;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public LocalDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+	public void setUpdatedDate(LocalDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public String getUserFirstName() {
+		return userFirstName;
+	}
+	public void setUserFirstName(String userFirstName) {
+		this.userFirstName = userFirstName;
+	}
+	public String getUserLastName() {
+		return userLastName;
+	}
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
+}
--- src/main/java/com/jspiders/ombs/entity/User.java
+++ src/main/java/com/jspiders/ombs/entity/User.java
@@ -1,8 +1,97 @@
 package com.jspiders.ombs.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "demo_user")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	private String userFirstName;
+	private String userLastName;
+	private String userRole;
+	private String userEmail;
+	private String userPassword;
+	@CreatedDate
+	private LocalDateTime createdDate;
+	@CreatedBy
+	private String createdBy;
+	@LastModifiedDate
+	private LocalDateTime updatedDate;
+	@LastModifiedBy
+	private String updatedBy;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public String getUserFirstName() {
+		return userFirstName;
+	}
+	public void setUserFirstName(String userFirstName) {
+		this.userFirstName = userFirstName;
+	}
+	public String getUserLastName() {
+		return userLastName;
+	}
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public LocalDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public void setUpdatedDate(LocalDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
 }
